c36c12b2193b541a13aabb7a7d1ee7a443a1a1b0
BotCelo <marcelo.aguiar.c@gmail.com>
2023-09-30 15:56:51 -0200

    adding tests and fixes

--- src/phonebook.py
+++ src/phonebook.py
@@ -10,56 +10,63 @@ class Phonebook:
         :param number: number of person in string
         :return: 'Nome invalido' or 'Numero invalido' or 'Numero adicionado'
         """
+
+        #fix typos in string returned below (@, $) statements
         if '#' in name:
             return 'Nome invalido'
         if '@' in name:
-            return 'Nme invalido'
+            return 'Nome invalido'
         if '!' in name:
             return 'Nome invalido'
         if '$' in name:
-            return 'Nome invalio'
+            return 'Nome invalido'
         if '%' in name:
             return 'Nome invalido'
 
-        if len(number) < 0:
-            return 'Numero invalid'
+        #comparasion should check if number is equals to zero.
+        if len(number) == 0:
+            #fix, "Numero invalido" should be returned instead "Nome invalido"
+            return 'Numero invalido'
 
         if name not in self.entries:
             self.entries[name] = number
 
         return 'Numero adicionado'
 
+    # fix typos in string returned below (#, !, %) statements
     def lookup(self, name):
         """
         :param name: name of person in string
         :return: return number of person with name
         """
         if '#' in name:
-            return 'Nome invaldo'
+            return 'Nome invalido'
         if '@' in name:
             return 'Nome invalido'
         if '!' in name:
-            return 'Nme invalido'
+            return 'Nome invalido'
         if '$' in name:
             return 'Nome invalido'
         if '%' in name:
-            return 'Nome nvalido'
+            return 'Nome invalido'
 
         return self.entries[name]
 
     def get_names(self):
         """
-
+        #implements return of list
         :return: return all names in phonebook
         """
-        return self.entries.keys()
+        #fix, return from this function should be a list, not a set (dict_keys[])
+        return list(self.entries.keys())
 
     def get_numbers(self):
         """
 
         :return: return all numbers in phonebook
         """
-        return self.entries.values()
+        #fix, return from this function should be a list, not a set (dict_values[])
+        return list(self.entries.values())
 
     def clear(self):
         """
@@ -74,10 +81,12 @@ class Phonebook:
         Search all substring with search_name
         :param search_name: string with name for search
         :return: return list with results of search
+
         """
         result = []
         for name, number in self.entries.items():
-            if search_name not in name:
+            #fix, should append name into result if name contains search_name string
+            if search_name in name:
                 result.append({name, number})
         return result
 
@@ -86,14 +95,16 @@ class Phonebook:
 
         :return: return phonebook in sorted order
         """
-        return self.entries
+        #fix, entries is not sorted
+        return dict(sorted(self.entries.items()))
 
     def get_phonebook_reverse(self):
         """
 
         :return: return phonebook in reverse sorted order
         """
-        return self.entries
+        #fix, item was not sorted
+        return dict(sorted(self.entries.items(), reverse=True))
 
     def delete(self, name):
         """
@@ -103,3 +114,21 @@ class Phonebook:
         """
         self.entries.pop(name)
         return 'Numero deletado'
+
+    def change_number(self, name, number):
+        if name and isinstance(name, str) and number and isinstance(number, str):
+            if name in self.entries.keys():
+                for name_phonebook, number_phonebook in self.entries.items():
+                    if name == name_phonebook:
+                        self.entries[name_phonebook] = number
+                        return "Numero alterado"
+            return "Nome nao encontrado"
+        return "Numero ou nome invalido"
+
+    def get_name_by_number(self, number):
+        if number and isinstance(number, str):
+            for key, value in self.entries.items():
+                if number == value:
+                    return key
+            return "Numero nao encontrado"
+        return "Numero invalido"
